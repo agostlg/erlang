92aa000c10149e84a0679dafe646c6a9f37eb21f
Thiago Carvalho <thiago.carvalho@westwing.com.br>
2013-11-06 08:56:09 -0200

    adicionei 00-helloworld

---
+++ 00-helloworld/hello.erl
@@ -0,0 +1,22 @@
+% definicao dos modulos
+-module(hello).
+
+% incluindo lib de testes unitarios
+-include_lib("eunit/include/eunit.hrl").
+
+% export
+-export([get_greeting/0]).
+-export([hello_world/0]).
+
+% implementacao
+get_greeting() -> throw("ERRO: nao implementado\n").
+hello_world()  -> io:fwrite(get_greeting()).
+
+% testes
+get_greeting_test() -> ?assert(get_greeting() =:= "hello, world\n").
+
+% compilando e rodando
+% erl -compile hello && erl -noshell -s hello hello_world -s init stop
+%
+% rodando testes
+% erl -compile hello && erl -noshell -s hello test -s init stop
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-erlang
+Aprendendo Erlang
 ======
+
+Dentro dos diretórios temos arquivos .erl com métodos ainda não implementados.
+
+Implemente os métodos de forma que os testes unitários passem.
+
+## Referências
+
+* [getting started](http://www.erlang.org/faq/getting_started.html#id52506)
+* [learn you some Erlang](http://learnyousomeerlang.com/content)
+* [documentação do EUnit](http://www.erlang.org/doc/apps/eunit/chapter.html)
